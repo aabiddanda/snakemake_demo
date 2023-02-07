0353d0637eafdd248ab61b54a55aafd9f8443e56
Arjun Biddanda <aabiddanda@gmail.com>
2023-02-07 09:48:02 -0500

    feat: snakemake should actually work now

--- workflow/scripts/sim_msprime.py
+++ workflow/scripts/sim_msprime.py
@@ -1,11 +1,18 @@
+"""Simulate a very simple msprime simulation and store."""
 
 import msprime
 import tszip
 
 if __name__ == "__main__":
-    n = snakemake.params['n'] #noqa
-    sequence_length = snakemake.params['len'] #noqa
-    seed = snakemake.params['seed'] #noqa
-    ts = msprime.sim_ancestry(n, sequence_length=sequence_length, population_size=1e4, recombination_rate=1e-8, random_seed=seed)
+    n = snakemake.params["n"]  # noqa
+    sequence_length = snakemake.params["len"]  # noqa
+    seed = snakemake.params["seed"]  # noqa
+    ts = msprime.sim_ancestry(
+        n,
+        sequence_length=sequence_length * 1e6,
+        population_size=1e4,
+        recombination_rate=1e-8,
+        random_seed=seed,
+    )
     mts = msprime.sim_mutations(ts, rate=1.2e-8, random_seed=seed)
-    tszip.compress(mts, snakemake.output['tsz'])
+    tszip.compress(mts, snakemake.output["tsz"])  # noqa
